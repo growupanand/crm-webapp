3b293642c39a8547a40492749359b951ad107da7
Utkarsh Anand <growupanand@gmail.com>
2023-01-10 00:06:19 +0530

    password will be saved in hashed format

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
 import User from "@models/user";
 import { sendMongooseErrorResponse } from "@utils/index";
+import bcrypt from "bcrypt";
 
 const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
   // create new user in database
   const newUser = new User({
     name,
     email,
-    password,
+    password : hashedPassword,
   });
   newUser.save((saveErr, savedData) => {
     if (saveErr) {
