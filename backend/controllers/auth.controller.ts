bd74e0e7d32e633c2cc0fb3de5508ea2fe061cec
Utkarsh Anand <growupanand@gmail.com>
2023-01-15 17:09:58 +0530

    added login route and controller

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -29,4 +29,30 @@ const register = async (req: Request, res: Response) => {
   });
 };
 
-export default { register };
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const errMessage = "Username or password is incorrect";
+  // check if user exist with provided email
+  const user = await User.findOne({ email });
+  if (!user) return res.sendCustomErrorMessage(errMessage, 400);
+
+  // check if password provided is correct
+  bcrypt.compare(password, user.password, (error, result) => {
+    if (error || !result) {
+      return res.sendCustomErrorMessage(errMessage, 400);
+    }
+    // create access token
+    const payload = {
+      name: user.name,
+      email: user.email,
+    };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.ACCESS_TOKEN_SECRET as string
+    );
+    return res.status(200).json({ accessToken });
+  });
+};
+
+export default { register, login };
--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import AuthController from "@controllers/auth.controller";
 
 router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
 
 module.exports = router;
