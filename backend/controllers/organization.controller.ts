5c87b083e0f7368bdfd148845f285888cc76ccf7
Utkarsh Anand <growupanand@gmail.com>
2023-01-26 17:04:14 +0530

    Now only user can delete organization which he created

--- backend/controllers/organization.controller.ts
+++ backend/controllers/organization.controller.ts
@@ -6,6 +6,11 @@ const {
   Types: { ObjectId },
 } = require("mongoose");
 
+/**
+ * Create new organization
+ * @param req
+ * @param res
+ */
 const createOrganization = async (req: Request, res: Response) => {
   const { name } = req.body;
   const newOrganization = new organizationModel({
@@ -20,6 +25,12 @@ const createOrganization = async (req: Request, res: Response) => {
   });
 };
 
+/**
+ * Get list of all organizations created by user
+ * @param req
+ * @param res
+ * @returns
+ */
 const getUserOrganizations = async (req: Request, res: Response) => {
   const organizations = await organizationModel.find({
     userId: req.user._id,
@@ -29,18 +40,32 @@ const getUserOrganizations = async (req: Request, res: Response) => {
   });
 };
 
+/**
+ * Delete an organization
+ * @param req
+ * @param res
+ * @returns
+ */
 const deleteOrganization = async (req: Request, res: Response) => {
   const { organizationId } = req.params;
-  try {
-    await organizationModel.deleteOne({
-      _id: ObjectId(organizationId),
-    });
+  const organization = await organizationModel.findOne({
+    _id: ObjectId(organizationId),
+  });
+  // check if organization exist in database
+  if (!organization)
+    return res.sendCustomErrorMessage("Organization not found");
+  // check if organization is created by current logged in user
+  if (String(organization.userId) !== String(req.user._id))
+    return res.sendCustomErrorMessage(
+      "You are not authorized to delete this organization"
+    );
+  // delete orgniazation from database
+  await organization.delete((error) => {
+    if (error) return res.sendMongooseErrorResponse(error);
     return res
       .status(200)
       .json({ message: "Organization deleted successfully" });
-  } catch (error) {
-    return res.sendMongooseErrorResponse(error as MongooseError);
-  }
+  });
 };
 
 export default { createOrganization, getUserOrganizations, deleteOrganization };
