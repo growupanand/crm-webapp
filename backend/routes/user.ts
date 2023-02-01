22713de08a79cb18d35da593e8c501ba3cb9c3e7
Utkarsh Anand <growupanand@gmail.com>
2023-02-01 23:40:55 +0530

    User can now delete his account

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import userController from "@controllers/user.controller";
 
 router.get("/:userId", userController.getUser);
+router.delete("/me", userController.deleteUser);
 
 module.exports = router;
