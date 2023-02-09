01ec0705ae83e104b4251831d5f572b69ad77a9b
Utkarsh Anand <growupanand@gmail.com>
2023-02-10 00:25:21 +0530

    Added nodemailer to send mails from our app

--- backend/index.ts
+++ backend/index.ts
@@ -1,23 +1,30 @@
 import express from "express";
-const app = express();
 import router from "./routes";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import baseMiddleware from "@middlewares/base.middleware";
 import authMiddleware from "@app/middlewares/auth.middleware";
+import mailMiddleware from "./middlewares/mail.middleware";
 
 dotenv.config();
 const port = process.env.PORT || 3001;
 
-app.use("/api/auth", express.json(), baseMiddleware, require("@routes/auth"));
+// ---------------EXPRESS JS----------------
+const app = express();
+app.use(
+  "/api/auth",
+  express.json(),
+  baseMiddleware,
+  mailMiddleware,
+  require("@routes/auth")
+);
 app.use("/api", express.json(), baseMiddleware, authMiddleware, router);
-
-// start server
 console.log(`Starting Backend Server: http://localhost:${port}`);
 app.listen(port);
-console.log("%cSuccess", "color: green");
+console.log("%cServer started successfully", "color: green");
+// ===========================================>
 
-// Connect Database
+// ---------------DATABASE------------------
 const databaseURI =
   process.env.MONGO_URI || "mongodb://127.0.0.1:27017/crm-webapp";
 mongoose.connect(databaseURI);
@@ -26,4 +33,7 @@ const db = mongoose.connection;
 db.on("error", (error) =>
   console.error("Error while connecting database", error)
 );
-db.once("open", () => console.log("%cSuccess", "color: green"));
+db.once("open", () =>
+  console.log("%cDatabase connected successfully", "color: green")
+);
+// ===========================================>
