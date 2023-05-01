fcdbf6e3594cf1bcff425514385e52a2b033f7f3
Utkarsh Anand <growupanand@gmail.com>
2023-05-01 23:04:08 +0530

    [backend] Expiring tokens will be deleted from the database automatically.

--- backend/models/token.ts
+++ backend/models/token.ts
@@ -1,5 +1,6 @@
 import { Token } from "@app/types/token";
 import { Schema, model } from "mongoose";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const tokenSchema = new Schema<Token>(
   {
@@ -21,11 +22,25 @@ const tokenSchema = new Schema<Token>(
       ref: "Organization",
       required: false,
     },
+    expiredAt: {
+      type: Date,
+      required: false,
+      expires: "0s", // this will delete document automatically on set date
+    },
   },
   {
     timestamps: true,
   }
 );
+
+tokenSchema.pre("save", function (next) {
+  // it will make token collection clean by automatically deleting all expired tokens itself
+  const tokenPayload = jwt.decode(this.token) as JwtPayload;
+  if (tokenPayload && tokenPayload.exp) {
+    this.expiredAt = new Date(tokenPayload.exp * 1000);
+  }
+  next();
+});
 
 const tokenModel = model<Token>("Token", tokenSchema);
 
