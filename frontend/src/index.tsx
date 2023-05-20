df4dcda7b04d2c51fd458f6fbfed0128a437edc0
Utkarsh Anand <growupanand@gmail.com>
2023-05-20 22:54:14 +0530

    [frontend] Created a folder structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./App";
+import App from "./views/App";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 
