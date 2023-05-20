753466d4b8d193e9504fe11d81d8d8ead6fca695
Utkarsh Anand <growupanand@gmail.com>
2023-05-20 17:49:58 +0530

    [frontend] Added images file Formats support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import "./Button.scss";
+import Logo from "./logo.svg";
 
 const App = () => {
   return (
     <div>
+      <img className="logo" src={Logo} />
       <h1>React App</h1>
       <p>This react app is created from scratch using webpack 5</p>
       <button>Test Button</button>
