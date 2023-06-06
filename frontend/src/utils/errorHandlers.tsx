66f4c3e5fd14e85daaa17dd4a30aff230c67efa3
Utkarsh Anand <growupanand@gmail.com>
2023-06-06 10:26:27 +0530

    [frontend] Added 'setFieldError' optional param in handleCachedError util, By passing this function, all field's error messages can be displayed next to the relevant fields in the form,  providing a better user experience.

--- frontend/src/utils/errorHandlers.tsx
+++ frontend/src/utils/errorHandlers.tsx
@@ -1,27 +1,46 @@
+import { SetFieldError } from "@mantine/form/lib/types";
 import { AxiosError } from "axios";
 
-export const getErrorMessage = (
+/**
+ *
+ * @param error - Cached error object
+ * @param fallbackErrorMessage - Display a custom message if nonFieldError not found in response
+ * @param setFieldError - This is a function that is used to set an error message for a specific field in a form.
+ * By passing this function, all field's error messages can be displayed next to the relevant fields in the form,
+ * providing a better user experience.
+ *
+ * @returns `void`
+ */
+export const handleCachedError = (
   error: AxiosError,
-  fallbackErrorMessage?: string
-) => {
+  fallbackErrorMessage?: string,
+  setFieldError?: SetFieldError<Record<string, any>>
+): void => {
   let errorMessage = fallbackErrorMessage || "Something went wrong";
   const responseData = error.response.data as Record<string, any>;
   const isErrorMessageExist =
     responseData && Object.keys(error.response.data).length > 0;
   if (!isErrorMessageExist) {
-    return errorMessage;
+    return alert(errorMessage);
   }
 
   const errorFields = Object.keys(responseData);
   const isNonFieldError = errorFields.includes("nonFieldError");
 
   if (isNonFieldError) {
-    return responseData.nonFieldError;
+    return alert(responseData.nonFieldError);
+  }
+
+  if (setFieldError) {
+    errorFields.forEach((errorField) => {
+      setFieldError(errorField, responseData[errorField]);
+    });
+    return;
   }
 
   errorMessage += "\n";
   errorFields.forEach((errorField) => {
     errorMessage += `${errorField} : ${responseData[errorField]}\n`;
   });
-  return errorMessage;
+  return alert(errorMessage);
 };
