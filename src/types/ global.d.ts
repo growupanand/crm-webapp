0a407d60af37aa8658bd28c1fdc915a1940674e8
growupanand <growupanand@users.noreply.github.com>
2023-07-08 09:30:27 +0000

    deploy: 23b5c8282948f384eeefabf951257e5a50ded1e0

--- src/types/ global.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-/**
- * Using this we do not need to import react on every file
- */
-
-declare global {
-  const React: typeof React;
-}
--- src/types/fileLoader.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * to solve type error while importing .svg file
- * Cannot find module './logo.svg' or its corresponding type declarations.ts(2307)
- */
-
-declare module "*.svg" {
-  const content: any;
-  export default content;
-}
