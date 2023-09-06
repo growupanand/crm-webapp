c87b2a5ecb91e4e53a85e26e7280e0f07b05ac10
Utkarsh Anand <growupanand@gmail.com>
2023-09-07 00:44:54 +0530

    [backend] Added rate limiter for a endpoint - change user password

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
 import userController from "@controllers/user.controller";
+import rateLimiter from "@app/middleware/rateLimiter.middleware";
 
 router.get("/:userId", userController.getUser);
 router.delete("/me", userController.deleteUser);
 router.post("/resendEmailVerification", userController.resendEmailVerify);
-router.post("/change-password", userController.changePassword);
+router.post(
+  "/change-password",
+  rateLimiter, // limit each IP to 2 requests per 5 seconds
+  userController.changePassword
+);
 
 module.exports = router;
