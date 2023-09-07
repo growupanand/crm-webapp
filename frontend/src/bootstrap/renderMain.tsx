3fb3d85fa95a6aa38184ba776c8e92face2058e3
Utkarsh Anand <growupanand@gmail.com>
2023-09-08 02:30:43 +0530

    [frontend] Added mantine notification and modal system, also added a notification store to easily manage notifications

--- frontend/src/bootstrap/renderMain.tsx
+++ frontend/src/bootstrap/renderMain.tsx
@@ -1,10 +1,15 @@
 import buildRoutes from "@app/routes";
 import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
 
 function RenderMainApp() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      {buildRoutes()}
+      <ModalsProvider>
+        <Notifications />
+        {buildRoutes()}
+      </ModalsProvider>
     </MantineProvider>
   );
 }
