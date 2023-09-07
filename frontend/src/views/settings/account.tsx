7d3711d4fae4c1ce6f4b59e3e237b6d4cfe4c254
Utkarsh Anand <growupanand@gmail.com>
2023-09-08 02:31:34 +0530

    [frontend] As a user, i can delete my account

--- frontend/src/views/settings/account.tsx
+++ frontend/src/views/settings/account.tsx
@@ -1,14 +1,62 @@
-import { getUser } from "@app/utils/storage";
-import { Table, Title } from "@mantine/core";
+import { Button } from "@app/components/button";
+import useAPIClient from "@app/hooks/useAPIClient";
+import { useAuthStore } from "@app/stores/authStore";
+import { addLoadingNotification } from "@app/stores/notificationStore";
+import { Group, Table, Text, Title } from "@mantine/core";
+import { modals } from "@mantine/modals";
+import { useState } from "react";
+
+type State = {
+  isDeleting: boolean;
+};
 
 function AccountPage() {
-  const user = getUser();
+  const [state, setState] = useState<State>({
+    isDeleting: false,
+  });
+  const { isDeleting } = state;
+
+  const { user, logout } = useAuthStore();
+  const { client } = useAPIClient();
+
+  const openDeleteModal = () =>
+    modals.openConfirmModal({
+      title: "Delete your account",
+      children: (
+        <Text size="sm">
+          Are you sure you want to delete your account? This action is not
+          reversible and all your data will be lost.
+        </Text>
+      ),
+      labels: {
+        confirm: "Yes delete my account",
+        cancel: "No don't delete it",
+      },
+      confirmProps: { color: "red" },
+      onConfirm: handleDeleteAccount,
+    });
+
+  const handleDeleteAccount = async () => {
+    setState((cs) => ({ ...cs, isDeleting: true }));
+    const closeNotification = addLoadingNotification("Deleting account");
+    try {
+      await client("/user/me", {
+        method: "DELETE",
+      });
+      closeNotification("Account deleted", "success");
+      logout();
+    } catch (error) {
+      closeNotification("Error deleting account", "error");
+      setState((cs) => ({ ...cs, isDeleting: false }));
+    }
+  };
+
   return (
     <>
       <Title order={3} mb="xs" fw="normal">
         Account
       </Title>
-      <Table>
+      <Table mb="xl">
         <tbody>
           <tr>
             <td>Name</td>
@@ -20,6 +68,16 @@ function AccountPage() {
           </tr>
         </tbody>
       </Table>
+      <Group>
+        <Button
+          variant="filled"
+          color="red"
+          onClick={openDeleteModal}
+          loading={isDeleting}
+        >
+          Delete Account
+        </Button>
+      </Group>
     </>
   );
 }
