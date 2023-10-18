607604a2f79603fd2e6dcfe9ce24706a7006afd8
Ravi <raviraj@advait.org.in>
2023-10-18 11:48:28 +0530

    addedmobileview

--- frontend/src/components/navTabs.tsx
+++ frontend/src/components/navTabs.tsx
@@ -1,4 +1,5 @@
-import { Box, NavLink } from "@mantine/core";
+import React, { useState } from "react";
+import { Box, NavLink, Text } from "@mantine/core";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ReactNode } from "react";
 
@@ -9,7 +10,7 @@ export type NavTab = {
   onClick?: () => void;
   Icon?: ReactNode;
   disabled?: boolean;
-  /** Overwrite function to check if current tab is active */
+  /** Overwrite function to check if the current tab is active */
   isActive?: (path: string) => boolean;
 };
 
@@ -25,33 +26,73 @@ const NavTabs = (props: Props) => {
   const navigate = useNavigate();
   const activeTab = tabs.find((tab) => location.pathname.includes(tab.path));
   const activeTabPath = activeTab?.path || "";
+
+  // State to manage the visibility of the mobile menu
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  // Toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!mobileMenuVisible);
+  };
+
   return (
-    <>
-      {tabs.map((tab) => (
-        <NavLink
-          key={`${tab.label}-${tab.path}`}
-          active={
-            tab.isActive !== undefined
-              ? tab.isActive(tab.path)
-              : activeTabPath === tab.path
-          }
-          label={tab.label}
-          onClick={() => {
-            onChange?.(tab.path);
-            tab.onClick?.();
-            tab.path && navigate(tab.path);
-          }}
-          icon={tab?.Icon}
-          disabled={props.disabled || tab.disabled}
-        >
-          {tab.nestedTabs && (
-            <Box mb={2}>
-              <NavTabs onChange={onChange} tabs={tab.nestedTabs} />
-            </Box>
-          )}
-        </NavLink>
-      ))}
-    </>
+    <nav>
+      <div className="desktop-nav">
+        {tabs.map((tab) => (
+          <NavLink
+            key={`${tab.label}-${tab.path}`}
+            active={
+              tab.isActive !== undefined
+                ? tab.isActive(tab.path)
+                : activeTabPath === tab.path
+            }
+            label={tab.label}
+            onClick={() => {
+              onChange?.(tab.path);
+              tab.onClick?.();
+              tab.path && navigate(tab.path);
+            }}
+            icon={tab?.Icon}
+            disabled={props.disabled || tab.disabled}
+          >
+            {tab.nestedTabs && (
+              <Box mb={2}>
+                <NavTabs onChange={onChange} tabs={tab.nestedTabs} />
+              </Box>
+            )}
+          </NavLink>
+        ))}
+      </div>
+      <div className="mobile-nav">
+        <button className="hamburger-menu" onClick={toggleMobileMenu}>
+          ☰
+        </button>
+        {mobileMenuVisible && (
+          <div className="mobile-menu">
+            {tabs.map((tab) => (
+              <NavLink
+                key={`${tab.label}-${tab.path}`}
+                active={
+                  tab.isActive !== undefined
+                    ? tab.isActive(tab.path)
+                    : activeTabPath === tab.path
+                }
+                label={tab.label}
+                onClick={() => {
+                  onChange?.(tab.path);
+                  tab.onClick?.();
+                  tab.path && navigate(tab.path);
+                  // Close the mobile menu after a selection
+                  setMobileMenuVisible(false);
+                }}
+                icon={tab?.Icon}
+                disabled={props.disabled || tab.disabled}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </nav>
   );
 };
 
